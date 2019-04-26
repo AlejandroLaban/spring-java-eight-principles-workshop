dffd1db0ccbf3d0eda28dd7b9f0f93ee23743a11
Alejandro Laban <alejandro.laban@gmail.com>
2019-04-26 17:35:49 -0500

    feat: add property configuration

--- spring-boot-all-in-one-demo/src/main/java/com/alejandrolaban/backend/principles/demo/config/BeanConfiguration.java
+++ spring-boot-all-in-one-demo/src/main/java/com/alejandrolaban/backend/principles/demo/config/BeanConfiguration.java
@@ -2,14 +2,22 @@ package com.alejandrolaban.backend.principles.demo.config;
 
 import com.alejandrolaban.backend.principles.demo.product.service.DummyProductServiceImpl;
 import com.alejandrolaban.backend.principles.demo.product.service.ProductService;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@EnableConfigurationProperties(ProductProperties.class)
 @Configuration
 public class BeanConfiguration {
 
+    private final ProductProperties productProperties;
+
+    public BeanConfiguration(ProductProperties productProperties) {
+        this.productProperties = productProperties;
+    }
+
     @Bean("beanDummyProductServiceImpl")
     ProductService productService() {
-        return new DummyProductServiceImpl();
+        return new DummyProductServiceImpl(productProperties);
     }
 }
